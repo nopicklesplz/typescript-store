97bc4471e7863f4ab65c2802efdee095b440ac59
Jesus Moreno <moreno_2318@yahoo.com>
2023-07-15 14:53:23 -0700

    Context Added

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
@@ -9,9 +10,17 @@ type StoreItemProps = {
 }
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-    const quantity = 2
+    const {
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart
+    } = useShoppingCart()
+
+    const quantity = getItemQuantity(id)
+
     return (
-        <Card>
+        <Card className='h-100'>
             <Card.Img
                 variant='top'
                 src={imgUrl}
@@ -25,7 +34,9 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                 </Card.Title>
                 <div className='mt-auto'>
                     {quantity === 0 ? (
-                        <Button className='w-100'>Add To Cart</Button>
+                        <Button className='w-100' onClick={() => increaseCartQuantity(id)}>
+                            + Add To Cart
+                        </Button>
                     ) : (
                         <div
                             className='d-flex align-items-center flex-column'
@@ -35,13 +46,18 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                                 className='d-flex align-items-center justify-content-center'
                                 style={{ gap: '.5rem' }}
                             >
-                                <Button className='rounded-circle'>-</Button>
+                                <Button className='rounded-circle' onClick={() => decreaseCartQuantity(id)}>
+                                    -
+                                </Button>
                                 <div>
-                                    <span className='fs-3'>{quantity}</span> in cart
+                                    <span className='fs-4'>{quantity}</span> in cart
                                 </div>
-                                <Button className='rounded-circle'>+</Button>
+                                <Button className='rounded-circle' onClick={() => increaseCartQuantity(id)}>
+                                    +
+                                </Button>
                             </div>
                             <Button
+                                onClick={() => removeFromCart(id)}
                                 variant='danger'
                                 size='sm'
                             >
